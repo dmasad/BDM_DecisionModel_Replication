ce3321809436bfa5a405459cb1561f65449a48bb
David Masad <david.masad@gmail.com>
2014-08-05 20:30:29 -0400

    Implemented some more steps

--- GroupDecisionModel.py
+++ GroupDecisionModel.py
@@ -33,12 +33,9 @@ class Actor(object):
         self.s = s
         self.r = 1
 
-        self.basic_utilities = defaultdict(lambda: 
-            {"Us": 0,
-            "Uf": 0,
-            "Ub": 0,
-            "Uw": 0,
-            "Usq": 0 })
+        self.basic_utilities = {}
+        self.probs = {}
+        self.expected_utilities = {}
 
 
     def calculate_utilities(self, alter):
@@ -59,13 +56,67 @@ class Actor(object):
                                             "Ub": Ub,
                                             "Uw": Uw,
                                             "Usq": Usq}
+    def calculate_prob(self, alter):
+        '''
+        Calculate probability of winning against an alter.
+        '''
+        # There won't be a contest against an alter with the same preference
+        if self.x == alter.x:
+            self.probs[alter.name] = 0
+            return
+
+        top = 0
+        bottom = 0
+        for agent in self.model.Actors:
+            d = (abs(agent.x - alter.x) - abs(agent.x - self.x))
+            if d > 0:
+                top += agent.c * agent.s * d
+            bottom += agent.c * agent.s * abs(d)
+        self.probs[alter.name] = top/bottom
+
+    def calculate_expected_utility(self, alter):
+        '''
+        Expected utility against a given alter
+        '''
+        # Local variables for ease of typing:
+        p = self.probs[alter.name]
+        utils = self.basic_utilities[alter.name]
+        Q = self.model.Q
+        T = self.model.T
+
+        EU = alter.s * (p * utils["Us"] + (1-p)*utils["Uf"]) + (1-alter.s)*utils["Us"]
+        EU -= Q*utils["Usq"] - (1-Q)*(T*utils["Ub"] + (1-T)*utils["Uw"])
+        self.expected_utilities[alter.name] = EU
+
+    def calculate_r(self):
+        '''
+        Calculate the risk exponent
+        '''
+
+        # Each other agent's expected utility of challenging this agent
+        attack_utils = [alter.expected_utilities[self.name] 
+            for alter in self.model.Actors if alter is not self]
+
+        security_levels = []
+        for actor in self.model.Actors:
+            security = sum([alter.expected_utilities[actor.name] 
+                    for alter in self.model.Actors if alter is not actor])
+            security_levels.append(security)
+
+        max_security = max(security_levels)
+        min_security = min(security_levels)
+        R = (2 * sum(attack_utils) - max_security - min_security) / (max_security - min_security)
+        self.r = (1 - R/3)/(1+R/3)
+
+
+
 
 class Model(object):
     '''
     A model container
     '''
 
-    def __init__(self, Actors, xmax, xmin):
+    def __init__(self, Actors, xmax, xmin, Q=1.0, T=1.0):
         '''
         Initiate a model with a list of Actor objects
         '''
@@ -75,6 +126,8 @@ class Model(object):
         self.xmin = xmin
         self.dx = xmax - xmin
         self.mu = 0 # Current median position
+        self.Q =  Q
+        self.T = T
 
     def vote(self, verbose=True):
         '''
